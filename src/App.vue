5700a18ea9d30e7577e03bced75400686e10f2eb
Vadym1202 <vadim.paliushek@gmail.com>
2022-03-19 11:15:09 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <CreateSurvey />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import CreateSurvey from "./modules/createSurvey/CreateSurvey.vue";
+
+@Options({
+  components: {
+    CreateSurvey,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/modules/createSurvey/CreateSurvey.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>Create survey</p>
+  </div>
+</template>
+<script lang="ts">
+import { Vue } from "vue-class-component";
+export default class CreateSurvey extends Vue {}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
